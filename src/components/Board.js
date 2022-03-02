4c32d5ed8417725ff489a242af2273c239fec128
aguentner <andreas.guentner@scandio.de>
2022-03-02 15:49:01 +0100

    feature: create basic setup for Coffee Cards
    - Render coffee cards based on list of ids
    - Fetch random data for names inside the coffee cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Button } from 'antd';
-const Board = () => (
-  <div className="board-wrapper">
-    <div className="board-add-icon">
-      <Button type="primary" >Add</Button>
+import { useState, useEffect } from "react";
+import { Button } from "antd";
+import Coffee from "./Coffee";
 
+const Board = () => {
+  const [coffees, setCoffess] = useState([1,2,3])
+  console.log(coffees)
+  return (
+    <div className="board-wrapper">
+      <div className="board-add-icon">
+        <Button type="primary">Add</Button>
+      </div>
+        {
+            coffees.map((coffee)=>(
+            <Coffee key={coffee} />
+        ))}
     </div>
-  </div>
-);
+  );
+};
 
 export default Board;
