4976c983018cf123ff98aba0cf4ad63a881381c9
aguentner <andreas.guentner@scandio.de>
2022-03-02 09:52:01 +0100

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from 'antd';
+const Board = () => (
+  <div className="board-wrapper">
+    <div className="board-add-icon">
+      <Button type="primary" >Add</Button>
+
+    </div>
+  </div>
+);
+
+export default Board;
