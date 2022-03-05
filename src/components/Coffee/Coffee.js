41cb1e663c756d3da7b7e51db1c84ea1c85dbb9d
anixel <45008199+andifg@users.noreply.github.com>
2022-03-05 20:35:29 +0100

    Feature: make single card rating editable

--- src/components/Coffee/Coffee.js
+++ src/components/Coffee/Coffee.js
@@ -1,19 +1,16 @@
 import { Card, Avatar, Skeleton, Rate } from "antd";
-import {
-  EditOutlined,
-  DeleteOutlined
-
-} from "@ant-design/icons";
+import { EditOutlined, DeleteOutlined, SaveOutlined } from "@ant-design/icons";
 import React from "react";
 import { useState, useEffect } from "react";
 
 const { Meta } = Card;
-import { getName} from "./CoffeeHelper";
+import { getName } from "./CoffeeHelper";
 
 const Coffee = (props) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [edit, setEdit] = useState(false);
 
   useEffect(async () => {
     try {
@@ -26,6 +23,35 @@ const Coffee = (props) => {
     }
   }, []);
 
+  const getActions = () => {
+    if (edit) {
+      return [
+        <SaveOutlined
+          key="saveChanges"
+          onClick={(e) => {
+            setEdit(false);
+            props.setdisableAdd(false)
+          }}
+        />,
+      ];
+    }
+    return [
+      <EditOutlined
+        key="edit"
+        onClick={(e) => {
+          setEdit(true);
+          props.setdisableAdd(true)
+        }}
+      />,
+      <DeleteOutlined
+        key="delete"
+        onClick={(e) => {
+          props.deleteCoffee(props.coffee);
+        }}
+      />,
+    ];
+  };
+
   return (
     <div className="coffee-wrapper">
       <Card
@@ -36,18 +62,13 @@ const Coffee = (props) => {
             src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
           />
         }
-        actions={[
-          <EditOutlined key="edit" />,
-          <DeleteOutlined key="delete" onClick={(e)=> {props.deleteCoffee(props.coffee)}} />
-        ]}
+        actions={getActions()}
       >
         <Skeleton loading={loading} avatar active>
-          <Meta
-            title={props.coffee}
-            description={data ? data : error}
-          />
+          <Meta title={props.coffee} />
           <div>
-            <Rate allowHalf defaultValue={2.5} disabled="true" />
+            <Rate allowHalf defaultValue={2.5} disabled={!edit} />
+            {edit && <p>Change the Rating here</p> }
           </div>
         </Skeleton>
       </Card>
--- src/components/Coffee/CoffeeHelper.js
+++ src/components/Coffee/CoffeeHelper.js
@@ -1,13 +1,7 @@
 
 
 const getName = async () => {
-      const response = await fetch(`https://randomuser.me/api/`);
-      if (!response.ok){
-        throw new Error('Error ');
-      }
-      let actualData = await response.json();
-      const completeName = `${actualData.results[0].name.first} ${actualData.results[0].name.last}`;
-      return completeName
+      
   };
 
 
